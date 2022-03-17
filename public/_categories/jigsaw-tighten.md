2ee82ba8b0689c37bc7ee2e59ecc0cbd033e2f9b
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-03-17 14:02:00 -0700

    let's add category support

---
+++ public/_categories/jigsaw-tighten.md
@@ -0,0 +1,3 @@
+---
+title: "Jigsaw Tighten"
+---
--- public/_layouts/posts.blade.php
+++ public/_layouts/posts.blade.php
@@ -3,17 +3,23 @@
 @section( 'body' )
     <section id="content" class="site-content">
         <div class="content-area">
-        <article class="post">
-                    <header class="entry-header">
-                        <div class="entry-metadata">
-                            <span class="entry-date">{{ $page->getDate()->format( 'F d, Y' ) }}</span>
-                        </div>
-                        <h1 class="entry-title"><a href="{{ $page->getUrl() }}">{{ $page->title }}</a></h1>
-                    </header>
-                    <div class="entry-content">
-                        @yield( 'content' )
+            <article class="post">
+                <header class="entry-header">
+                    <div class="entry-metadata">
+                        <span class="entry-date">{{ $page->getDate()->format( 'F d, Y' ) }}</span>
                     </div>
-                </article>
+                    <h1 class="entry-title"><a href="{{ $page->getUrl() }}">{{ $page->title }}</a></h1>
+                </header>
+                <div class="entry-content">
+                    @yield( 'content' )
+                </div>
+                <div class="entry-footer">
+                    Topics:
+                    @foreach ($page->categories as $category )
+                        <a href="/category/{{ $category }}">#{{ $category }}</a> 
+                    @endforeach
+                </div>
+            </article>
         </div>
     </section>
 @endsection

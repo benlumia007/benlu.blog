982e50f882a7883e9f607a774fec467bffa11a4a
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-03-17 11:20:24 -0700

    removed foreach sicne we don't need to loop the content for 404 page.

--- public/_layouts/404.blade.php
+++ public/_layouts/404.blade.php
@@ -3,16 +3,14 @@
 @section( 'body' )
     <section id="content" class="site-content">
         <div class="content-area">
-            @foreach ( $posts as $post )
-                <article class="post">
-                    <header class="entry-header">
-                        <h1 class="entry-title">{{ $page->title }}</a></h1>
-                    </header>
-                    <div class="entry-content">
-                        @yield( 'content' )
-                    </div>
-                </article>
-            @endforeach
+            <article class="post">
+                <header class="entry-header">
+                    <h1 class="entry-title">{{ $page->title }}</a></h1>
+                </header>
+                <div class="entry-content">
+                    @yield( 'content' )
+                </div>
+            </article>
         </div>
     </section>
 @endsection

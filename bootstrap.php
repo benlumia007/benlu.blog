0178b55d5ab709be8c6f0ff71ede58b643868e8b
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-03-18 10:46:35 -0700

    remove

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,3 @@
 <?php
 
-$events->afterBuild(App\Listeners\GenerateSitemap::class);
-$events->afterBuild(App\Listeners\GenerateIndex::class);
-
 App\Listeners\AddArchivePages::register( $container );

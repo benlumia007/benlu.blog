782009f6be907f667608425220cbd5fad14fae68
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-03-18 10:16:56 -0700

    updated

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,6 @@
 <?php
 
 $events->afterBuild(App\Listeners\GenerateSitemap::class);
+$events->afterBuild(App\Listeners\GenerateIndex::class);
 
 App\Listeners\AddArchivePages::register( $container );

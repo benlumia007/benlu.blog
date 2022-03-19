81a0396e8810a5191e8117a192d96a9c59412034
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-03-18 18:07:28 -0700

    initial commit

---
+++ public/_views/category.blade.php
@@ -0,0 +1,21 @@
+@extends( '_layouts.main' )
+
+@section( 'body' )
+<section id="content" class="site-content">
+    <div class="content-area">
+        <article class="page">
+            <header class="entry-header">
+                <h1 class="entry-title">{{ $page->title }}</a></h1>
+            </header>
+            <div class="entry-content">
+                @yield( 'content' )
+            </div>
+            <ul>
+                @foreach ( $posts->filter->hasCategory( $page->name() ) as $post )
+                    <li><a href="{{ $post->getPath() }}">{{ $post->title }}</a></li>
+                @endforeach
+            </ul>
+        </article>
+    </div>
+</section>
+@endsection

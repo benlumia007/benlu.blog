28f3c2666e5fcc0d63553199cdbda277144bafda
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-03-18 19:32:19 -0700

    updated

---
+++ public/_views/archive-year.blade.php
@@ -0,0 +1,23 @@
+@extends( '_layouts.main' )
+
+@section( 'body' )
+<section id="content" class="site-content">
+    <div class="content-area">
+        <div class="archive-header">
+            <h1 class="archive-title">{{ \Carbon\Carbon::createFromDate( $page->year, 1 )->format( 'Y' ) }}</h1>
+        </div>
+        @foreach ( $page->getPostsForYear( $posts ) as $post )
+            <article class="post">
+                <header class="entry-header">
+                    <h1 class="entry-title"><a href="{{ $post->getUrl() }}">{{ $post->title }}</a></h1>
+                </header>
+                <div class="entry-content">
+                    <p>
+                        {!! $post->getExcerpt() !!}
+                    </p>
+                </div>
+            </article>
+        @endforeach
+    </div>
+</section>
+@endsection
